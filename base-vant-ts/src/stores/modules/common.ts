d5ed1608c6f079db52c01acbcf60d21443b98c00
张宇航 <1401263323qq.com>
2023-08-23 10:04:42 +0800

    http封装

--- base-vant-ts/src/stores/modules/common.ts
+++ base-vant-ts/src/stores/modules/common.ts
@@ -4,14 +4,21 @@ import { store } from '../index'
 interface commonType {
   test: string | number
   testData: AnyObject
+  networkState: Boolean
 }
 
 export const userCommonStore = defineStore({
   id: 'common',
   state: (): commonType => ({
     test: '123',
-    testData: {}
-  })
+    testData: {},
+    networkState: true
+  }),
+  actions: {
+    setNetwork(flag: boolean) {
+      this.networkState = flag
+    }
+  }
 })
 
 export function userCommonStoreHook() {
