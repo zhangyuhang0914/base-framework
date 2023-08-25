e2b833199d0a59c265c80b994426537d31081a69
张宇航 <1401263323qq.com>
2023-08-25 10:38:17 +0800

    监听网络变化

--- base-vant-ts/src/App.vue
+++ base-vant-ts/src/App.vue
@@ -1,11 +1,33 @@
 <template lang="pug">
 router-view(v-slot="{ Component }")
   component(:is="Component")
-  span(class="adb") {{ 'asdb' }}
-  //- van-button {{ '123' }}
 </template>
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { onUnmounted } from 'vue'
+import { showNotify } from 'vant'
+import { userCommonStoreHook } from '@/stores/modules/common'
+let noticyObj: any = null
+const updateOnline = () => {
+  const commonHook = userCommonStoreHook()
+  const onlineState = !!navigator.onLine
+  if (!onlineState) {
+    noticyObj = showNotify({
+      type: 'danger',
+      message: '网络连接已断开',
+      duration: 0
+    })
+  } else {
+    noticyObj && noticyObj.closeNotify()
+  }
+  commonHook.setNetwork(onlineState)
+}
+// 监听网络变化
+window.addEventListener('online', updateOnline)
+window.addEventListener('offline', updateOnline)
+onUnmounted(() => {
+  window.removeEventListener('online', updateOnline)
+  window.removeEventListener('offline', updateOnline)
+})
 </script>
 
 <style lang="stylus">
