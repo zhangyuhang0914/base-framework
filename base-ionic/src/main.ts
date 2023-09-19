35ba298c429d4ef4f2c4ab8dcea4abca63cdd6aa
张宇航 <1401263323qq.com>
2023-09-19 10:23:58 +0800

    初始化

--- base-ionic/src/main.ts
+++ base-ionic/src/main.ts
@@ -5,6 +5,7 @@ import { IonicVue } from '@ionic/vue'
 import store from './stores/index'
 import { getConf } from '@/conf/conf'
 import { directivesHook } from '@/directives/index'
+import { SplashScreen } from '@capacitor/splash-screen'
 import '@/plugins/flexible'
 import '@/assets/import-ionic-styl'
 
@@ -14,8 +15,14 @@ getConf(app).then(async config => {
   app.use(router)
   app.use(store)
   // ionic路由创建后挂载到页面
+  app.mount('#app').$nextTick(() => {
+    SplashScreen.show({
+      autoHide: true, // 是否自动隐藏
+      fadeInDuration: 200 // 渐隐动画时长
+    })
+  })
   router.isReady().then(() => {
-    app.mount('#app')
+    SplashScreen.hide()
   })
   // 指令
   directivesHook(app)
