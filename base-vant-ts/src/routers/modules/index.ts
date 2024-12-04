981f85502b360515a76ce92755e447e29689624e
张宇航 <1401263323qq.com>
2024-12-04 11:31:41 +0800

    vant模板调整

--- base-vant-ts/src/routers/modules/index.ts
+++ base-vant-ts/src/routers/modules/index.ts
@@ -2,7 +2,7 @@ import type { AppRouteModule } from '@/routers/types'
 
 const routers: AppRouteModule[] = [
   {
-    path: 'entry',
+    path: '/entry',
     name: 'Entry',
     component: () => import('@/views/entry/index.vue'),
     meta: {
