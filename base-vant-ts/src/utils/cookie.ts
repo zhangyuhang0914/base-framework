16a42f1fa91d0c4c21b4c11ad3cea1d0c0371d82
张宇航 <1401263323qq.com>
2024-12-09 15:31:57 +0800

    vant模板优化cookie存储

--- base-vant-ts/src/utils/cookie.ts
+++ base-vant-ts/src/utils/cookie.ts
@@ -3,21 +3,27 @@
  * @Autor        : ZhangYuHang
  * @Date         : 2024-12-03 18:06:07
  * @LastEditors  : ZhangYuHang
- * @LastEditTime : 2024-12-04 11:23:51
+ * @LastEditTime : 2024-12-09 15:31:39
  */
 
 import Cookies from 'js-cookie'
 
-const TokenKey = 'Admin-Token'
-
-export function getToken(): any {
-  return Cookies.get(TokenKey)
+const CookieKeys = {
+  TokenKey: 'Admin-Token',
+  JsessionKey: 'JSESSIONID'
+}
+// key值类型
+type CookieKeyType = keyof typeof CookieKeys
+// 具体值类型
+type CookieKeyValues = (typeof CookieKeys)[CookieKeyType]
+export function getToken(cookieKey: CookieKeyValues): any {
+  return Cookies.get(cookieKey)
 }
 
-export function setToken(token: string, expires?: number | Date): void {
-  Cookies.set(TokenKey, token, { expires: expires })
+export function setToken(token: string, expires: number | Date, cookieKey: CookieKeyValues): void {
+  Cookies.set(cookieKey, token, { expires: expires })
 }
 
-export function removeToken(): void {
-  Cookies.remove(TokenKey)
+export function removeToken(cookieKey: CookieKeyValues): void {
+  Cookies.remove(cookieKey)
 }
